f2d5d2f5429ac3046fd3072fc9792f74d4ee349a
Ian Dees <undees@gmail.com>
2010-05-10 13:05:16 -0700

    Cleanup

--- touchscreen/features/step_definitions/waveform_steps.rb
+++ touchscreen/features/step_definitions/waveform_steps.rb
@@ -3,13 +3,14 @@ Given(/^a waveform$/) do
 end
 
 When(/^I set the Waveform Type to "([^\"]*)"$/) do |value|
-  control = const_get "IDC_#{value.upcase}" #<1>
+  # Control IDs like IDC_SINE are integers defined in the C app
+  control = const_get "IDC_#{value.upcase}"
   result  = get("/ClickControl?parent=#{@win}&control=#{control}").to_i
   result.should == 1
 end
 
 Then(/^the Duty Cycle setting should be (.*)$/) do |state|
-  control  = IDC_DUTY_CYCLE
+  control  = IDC_DUTY_CYCLE # an integer defined in the C app
   actual   = get("/IsControlEnabled?parent=#{@win}&control=#{control}").to_i
   expected = (state == 'enabled' ? 1 : 0)
 
