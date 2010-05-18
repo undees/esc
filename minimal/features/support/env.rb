79012e774a6bdfe5d9f90ffd5ed5c9b8530ee009
Ian Dees <undees@gmail.com>
2010-05-18 14:40:37 -0700

    Get working with IronRuby

--- minimal/features/support/env.rb
+++ minimal/features/support/env.rb
@@ -1,6 +1,8 @@
 require 'serialport'
 
 class MoodWorld
+  @@port = nil
+
   Emotions = %w(furious unhappy neutral happy ecstatic)
 
   def int_for(emotion)
@@ -8,13 +10,20 @@ class MoodWorld
   end
 
   def port
-    return @port if @port
+    return @@port if @@port
 
-    @port = SerialPort.new(ENV['MOOD_HAT'], 9600, 8, 1, SerialPort::NONE)
-    @port.read_timeout = 1000
-    @port.putc '?' until @port.read =~ /\d/
-    @port
+    @@port = SerialPort.new(ENV['MOOD_HAT'], 9600, 8, 1, SerialPort::NONE)
+    @@port.read_timeout    = 2000
+    @@port.wait_after_send = 1000 if @port.methods.include? :wait_after_send=
+
+    @@port.putc('?') until (@@port.read =~ /\d/ rescue nil)
+    @@port
+  end
+
+  def self.close
+    @@port.close if @@port
   end
 end
 
-World { MoodWorld.new }
+World   { MoodWorld.new   }
+at_exit { MoodWorld.close }
